c6ac2393703a4b39383a033df94cf55c89a089c4
Alex Launi <alaun001@odu.edu>
2021-04-14 12:05:55 -0400

    load clearview db connection params or local

--- api/app/config.php
+++ api/app/config.php
@@ -11,13 +11,22 @@ use Monolog\Handler\StreamHandler;
 use CS450\Service\JwtService;
 use CS450\Service\DbService;
 
-return [
+function getDbConnectionDetailsArray() {
+    $cleardb_url = getenv("CLEARDB_DATABASE_URL");
+    $cleardb_conn_params = parse_url($cleardb_url);
+
+    return array(
+        "db.host" => empty($cleardb_url) ? $cleardb_conn_params["host"] : getenv("MYSQL_HOST"),
+        "db.user" => empty($cleardb_url) ? $cleardb_conn_params["user"] : getenv("MYSQL_USER"),
+        "db.pass" => empty($cleardb_url) ? $cleardb_conn_params["pass"] : getenv("MYSQL_PASSWORD"),
+        "db.name" => empty($cleardb_url) ? substr($cleardb_conn_params["path"], 1) : getenv("MYSQL_DATABASE"),
+    );
+}
+
+return array_merge(
+[
     "env" => "dev",
     "jwt.key" => "5f2b5cdbe5194f10b3241568fe4e2b24",
-    "db.host" => getenv("MYSQL_HOST"),
-    "db.user" => getenv("MYSQL_USER"),
-    "db.name" => getenv("MYSQL_DATABASE"),
-    "db.password" => getenv("MYSQL_PASSWORD"),
     DbService::class => DI\Autowire(CS450\Service\Db\MysqlDb::class),
     JwtService::class => create(CS450\Service\Jwt\FirebaseJwt::class),
     Psr\Log\LoggerInterface::class => DI\factory(function () {
@@ -30,4 +39,6 @@ return [
 
         return $logger;
     }),
-];
+],
+getDbConnectionDetailsArray()
+);
