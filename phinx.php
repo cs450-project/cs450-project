236554cbf06e63b7e304ae8dc92cda4cd7d2abe0
Alex Launi <dev@launi.me>
2021-04-14 18:37:25 -0400

    update phinx config and run migration after build

--- phinx.php
+++ phinx.php
@@ -1,5 +1,8 @@
 <?php
 
+$cleardb_uri = getenv("CLEARDB_DATABASE_URL");
+$cleardb_conn_params = parse_url($cleardb_uri);
+
 return
 [
     'paths' => [
@@ -8,13 +11,22 @@ return
     ],
     'environments' => [
         'default_migration_table' => 'phinxlog',
-        'default_environment' => 'development',
+        'default_environment' => 'staging',
         'production' => [
             'adapter' => 'mysql',
             'host' => 'localhost',
             'name' => 'production_db',
             'user' => 'root',
             'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'staging' => [
+            'adapter' => 'mysql',
+            'host' => $cleardb_conn_params['host'],
+            'name' => substr($cleardb_conn_params['path'], 1),
+            'user' => $cleardb_conn_params['user'],
+            'pass' => $cleardb_conn_params['pass'],
             'port' => '3306',
             'charset' => 'utf8',
         ],
