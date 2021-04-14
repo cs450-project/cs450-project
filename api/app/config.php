99efdcc7013eb828e2313a7ffcd8f8592c710204
Alex Launi <alaun001@odu.edu>
2021-04-14 12:27:06 -0400

    logic error for chosing db url parse or env vars

--- api/app/config.php
+++ api/app/config.php
@@ -14,31 +14,30 @@ use CS450\Service\DbService;
 function getDbConnectionDetailsArray() {
     $cleardb_url = getenv("CLEARDB_DATABASE_URL");
     $cleardb_conn_params = parse_url($cleardb_url);
-
     return array(
-        "db.host" => empty($cleardb_url) ? $cleardb_conn_params["host"] : getenv("MYSQL_HOST"),
-        "db.user" => empty($cleardb_url) ? $cleardb_conn_params["user"] : getenv("MYSQL_USER"),
-        "db.password" => empty($cleardb_url) ? $cleardb_conn_params["pass"] : getenv("MYSQL_PASSWORD"),
-        "db.name" => empty($cleardb_url) ? substr($cleardb_conn_params["path"], 1) : getenv("MYSQL_DATABASE"),
+        "db.host" => !empty($cleardb_url) ? $cleardb_conn_params["host"] : getenv("MYSQL_HOST"),
+        "db.user" => !empty($cleardb_url) ? $cleardb_conn_params["user"] : getenv("MYSQL_USER"),
+        "db.password" => !empty($cleardb_url) ? $cleardb_conn_params["pass"] : getenv("MYSQL_PASSWORD"),
+        "db.name" => !empty($cleardb_url) ? substr($cleardb_conn_params["path"], 1) : getenv("MYSQL_DATABASE"),
     );
 }
 
 return array_merge(
-[
-    "env" => "dev",
-    "jwt.key" => "5f2b5cdbe5194f10b3241568fe4e2b24",
-    DbService::class => DI\Autowire(CS450\Service\Db\MysqlDb::class),
-    JwtService::class => create(CS450\Service\Jwt\FirebaseJwt::class),
-    Psr\Log\LoggerInterface::class => DI\factory(function () {
-        $logger = new Logger("CS450");
+    getDbConnectionDetailsArray(),
+    [
+        "env" => "dev",
+        "jwt.key" => "5f2b5cdbe5194f10b3241568fe4e2b24",
+        DbService::class => DI\Autowire(CS450\Service\Db\MysqlDb::class),
+        JwtService::class => create(CS450\Service\Jwt\FirebaseJwt::class),
+        Psr\Log\LoggerInterface::class => DI\factory(function () {
+            $logger = new Logger("CS450");
 
-        $fileHandler = new StreamHandler("php://stdout", Logger::DEBUG);
-        $logger->pushHandler($fileHandler);
+            $fileHandler = new StreamHandler("php://stdout", Logger::DEBUG);
+            $logger->pushHandler($fileHandler);
 
-        ErrorHandler::register($logger);
+            ErrorHandler::register($logger);
 
-        return $logger;
-    }),
-],
-getDbConnectionDetailsArray()
+            return $logger;
+        }),
+    ]
 );
