aa8838db48a92cad77026d7207fc3e5547fb000d
Alex Launi <alaun001@odu.edu>
2021-04-14 12:35:27 -0400

    define db connection fn inline

--- api/app/config.php
+++ api/app/config.php
@@ -11,19 +11,17 @@ use Monolog\Handler\StreamHandler;
 use CS450\Service\JwtService;
 use CS450\Service\DbService;
 
-function getDbConnectionDetailsArray() {
-    $cleardb_url = getenv("CLEARDB_DATABASE_URL");
-    $cleardb_conn_params = parse_url($cleardb_url);
-    return array(
-        "db.host" => !empty($cleardb_url) ? $cleardb_conn_params["host"] : getenv("MYSQL_HOST"),
-        "db.user" => !empty($cleardb_url) ? $cleardb_conn_params["user"] : getenv("MYSQL_USER"),
-        "db.password" => !empty($cleardb_url) ? $cleardb_conn_params["pass"] : getenv("MYSQL_PASSWORD"),
-        "db.name" => !empty($cleardb_url) ? substr($cleardb_conn_params["path"], 1) : getenv("MYSQL_DATABASE"),
-    );
-}
-
 return array_merge(
-    getDbConnectionDetailsArray(),
+    (function() {
+        $cleardb_url = getenv("CLEARDB_DATABASE_URL");
+        $cleardb_conn_params = parse_url($cleardb_url);
+        return array(
+            "db.host" => !empty($cleardb_url) ? $cleardb_conn_params["host"] : getenv("MYSQL_HOST"),
+            "db.user" => !empty($cleardb_url) ? $cleardb_conn_params["user"] : getenv("MYSQL_USER"),
+            "db.password" => !empty($cleardb_url) ? $cleardb_conn_params["pass"] : getenv("MYSQL_PASSWORD"),
+            "db.name" => !empty($cleardb_url) ? substr($cleardb_conn_params["path"], 1) : getenv("MYSQL_DATABASE"),
+        );
+    })(),
     [
         "env" => "dev",
         "jwt.key" => "5f2b5cdbe5194f10b3241568fe4e2b24",
