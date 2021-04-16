8c3a5abc94216f4a7ba5087aa1273f1e24260f2b
Alex Launi <dev@launi.me>
2021-04-15 20:59:07 -0400

    misnamed db_conn_param variable

--- phinx.php
+++ phinx.php
@@ -3,7 +3,6 @@
 include 'api/app/dbconfig.php';
 
 $db_conn_params = load_db_config();
-
 return
 [
     'paths' => [
@@ -24,10 +23,10 @@ return
         ],
         'staging' => [
             'adapter' => 'mysql',
-            'host' => $conn_params['host'],
-            'name' => $conn_params['name'],
-            'user' => $conn_params['user'],
-            'pass' => $conn_params['pass'],
+            'host' => $db_conn_params['host'],
+            'name' => $db_conn_params['name'],
+            'user' => $db_conn_params['user'],
+            'pass' => $db_conn_params['pass'],
             'port' => '3306',
             'charset' => 'utf8',
         ],
