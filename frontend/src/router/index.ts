bbb38d6252f5ee80d68871d142f5aa5d14c0d1ba
Alex Launi <alaun001@odu.edu>
2021-04-18 15:48:20 -0400

    Add view for inviting faculty to register
    Add view for inviting faculty to register
    Backend not implemented. Asks for faculty,
    email, and department which will be prefilled
    when the faculty visits the registration form.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from "vue-router";
 
 import Register from "@/views/Register.vue";
 import Login from "@/views/Login.vue";
+import Invite from "@/views/Invite.vue";
 
 Vue.use(VueRouter);
 
@@ -27,10 +28,28 @@ const routes: Array<RouteConfig> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/invite",
+    name: "Invite",
+    component: Invite,
+    meta: {
+      requiresAuth: true,
+    },
+  },
 ];
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem("authData");
+
+  if (to.matched.some((record) => record.meta?.requiresAuth) && !loggedIn) {
+    next("/");
+  }
+
+  next();
 });
 
 export default router;
